4d54d8a9224284693af589b51e8425d53e994c70
Trigo00 <trigotex@gmail.com>
2022-10-07 22:49:29 -0300

    ult err

--- src/main/java/com/portfolio/mgb/MgbApplication.java
+++ src/main/java/com/portfolio/mgb/MgbApplication.java
@@ -2,10 +2,8 @@ package com.portfolio.mgb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan
 public class MgbApplication {
 
 	public static void main(String[] args) {
